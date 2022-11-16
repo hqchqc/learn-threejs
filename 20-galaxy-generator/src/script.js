701927350bcfe957f6db1c97f452a13d7ce7696c
hqchqc <734986933@qq.com>
2022-11-16 22:44:33 +0800

    feat: 增加星系章节 下

--- 20-galaxy-generator/src/script.js
+++ 20-galaxy-generator/src/script.js
@@ -19,13 +19,15 @@ const scene = new THREE.Scene();
 
 // Galaxy
 const parameters = {
-  count: 1000,
+  count: 10000,
   size: 0.02,
   radius: 5,
   branches: 3,
   spin: 1,
   randomness: 0.2,
   randomnessPower: 3,
+  insideColor: "#ff6030",
+  outsideColor: "#1b3984",
 };
 // parameters.count = 1000;
 // parameters.size = 0.02;
@@ -44,10 +46,15 @@ const generateGalaxy = () => {
   geometry = new THREE.BufferGeometry();
 
   const position = new Float32Array(parameters.count * 3);
+  const colors = new Float32Array(parameters.count * 3);
+
+  const colorInside = new THREE.Color(parameters.insideColor);
+  const colorOutside = new THREE.Color(parameters.outsideColor);
 
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
 
+    // Position
     const radius = Math.random() * parameters.radius;
     const spinAngle = radius * parameters.spin;
     const branchAngle =
@@ -55,23 +62,30 @@ const generateGalaxy = () => {
 
     const randomX =
       Math.pow(Math.random(), parameters.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      parameters.randomness;
+      (Math.random() < 0.5 ? 1 : -1);
+
     const randomY =
       Math.pow(Math.random(), parameters.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      parameters.randomness;
+      (Math.random() < 0.5 ? 1 : -1);
+
     const randomZ =
       Math.pow(Math.random(), parameters.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      parameters.randomness;
+      (Math.random() < 0.5 ? 1 : -1);
 
-    position[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+    position[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX;
     position[i3 + 1] = randomY;
     position[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
+
+    // Color
+    const mixedColor = colorInside.clone();
+    mixedColor.lerp(colorOutside, radius / parameters.radius);
+    colors[i3 + 0] = mixedColor.r;
+    colors[i3 + 1] = mixedColor.g;
+    colors[i3 + 2] = mixedColor.b;
   }
 
   geometry.setAttribute("position", new THREE.BufferAttribute(position, 3));
+  geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
   /**
    * Material
@@ -81,6 +95,7 @@ const generateGalaxy = () => {
     sizeAttenuation: true,
     depthWrite: false,
     blending: THREE.AdditiveBlending,
+    vertexColors: true,
   });
 
   /**
@@ -134,6 +149,8 @@ gui
   .max(10)
   .step(0.001)
   .onFinishChange(generateGalaxy);
+gui.addColor(parameters, "insideColor").onFinishChange(generateGalaxy);
+gui.addColor(parameters, "outsideColor").onFinishChange(generateGalaxy);
 /**
  * Test cube
  */
